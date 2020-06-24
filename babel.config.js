03d518ea83341107f8196c642a892c23249b4db4
Jonathan Perry <jonathanperry@modernpoacher.com>
2020-06-24 19:03:55 +0100

    Babel

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,51 @@
-module.exports = {
-  compact: true,
-  comments: false,
-  presets: [
-    [
-      '@babel/env', {
-        useBuiltIns: 'entry',
-        targets: {
-          node: 'current',
-          browsers: [
-            'last 2 versions'
-          ]
-        },
-        corejs: 3
-      }
-    ],
-    '@babel/react'
+const debug = require('debug')
+
+const log = debug('shinkansen')
+
+const {
+  env: {
+    NODE_ENV = 'development'
+  }
+} = process
+
+log('`shinkansen` is awake')
+
+function env () {
+  log({ NODE_ENV })
+
+  return (
+    NODE_ENV === 'production'
+  )
+}
+
+const presets = [
+  [
+    '@babel/env', {
+      useBuiltIns: 'entry',
+      targets: {
+        node: 'current',
+        browsers: [
+          'last 2 versions'
+        ]
+      },
+      corejs: 3
+    }
   ],
-  plugins: [
-    '@babel/proposal-export-default-from',
-    '@babel/proposal-export-namespace-from'
-  ]
+  '@babel/react'
+]
+
+const plugins = [
+  '@babel/proposal-export-default-from',
+  '@babel/proposal-export-namespace-from'
+]
+
+module.exports = (api) => {
+  if (api) api.cache.using(env)
+
+  return {
+    compact: true,
+    comments: false,
+    presets,
+    plugins
+  }
 }
