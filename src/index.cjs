7aecfc9cd1e501139d1743cd253d8e9bf3c91e39
Jonathan Perry <jonathanperry@modernpoacher.com>
2023-05-11 13:33:09 +0100

    Re-orders imports and exports

--- src/index.cjs
+++ src/index.cjs
@@ -9,21 +9,21 @@ log('`shinkansen` is awake')
 const Cogs = require('shinkansen-cogs')
 const Sprockets = require('shinkansen-sprockets')
 const { Gears } = require('shinkansen-gears')
-const { Pantograph } = require('shinkansen-pantograph')
 const { Pinion } = require('shinkansen-pinion')
 const { Rails } = require('shinkansen-rails')
 const { Relay } = require('shinkansen-relay')
 const { Signals } = require('shinkansen-signals')
+const { Pantograph } = require('shinkansen-pantograph')
 const Transmission = require('shinkansen-transmission')
 const { Engine } = require('shinkansen-engine')
 
 module.exports.Cogs = Cogs
 module.exports.Sprockets = Sprockets
 module.exports.Gears = Gears
-module.exports.Pantograph = Pantograph
 module.exports.Pinion = Pinion
 module.exports.Rails = Rails
 module.exports.Relay = Relay
 module.exports.Signals = Signals
+module.exports.Pantograph = Pantograph
 module.exports.Transmission = Transmission
 module.exports.Engine = Engine
