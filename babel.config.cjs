a75ab18fbb93db852e5665d3cc670e499ba11a2e
Jonathan Perry <jonathanperry@modernpoacher.com>
2023-05-27 10:56:09 +0100

    Babel

--- babel.config.cjs
+++ babel.config.cjs
@@ -49,6 +49,8 @@ module.exports = (api) => {
   return {
     presets,
     plugins,
-    ignore: [/node_modules\/(?!shinkansen-)/]
+    ignore: [
+      /node_modules\/(?!shinkansen|@modernpoacher)/
+    ]
   }
 }
